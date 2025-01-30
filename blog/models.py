b586b129b3b519cbc3c1906061fbad055f3d7379
Your Name <mlrodya@MacBook-Pro-Mikhail.local>
2025-01-30 11:04:27 +0100

    first commit misha

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+
 from django.db import models
 
 class Post (models. Model):
@@ -6,6 +8,7 @@ class Post (models. Model):
     description = models.TextField('Текст записи')
     author = models.CharField('Имя автора', max_length=100)
     date = models.DateField('Дата публикации')
+    img = models.ImageField('Изображение', upload_to='image/%Y')
 
     def __str__(self):
         return f'{self.title}, {self.author}'
